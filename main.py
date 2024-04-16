abfb521d46a7447a46d35bbfdb9faba18fadce21
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-16 21:08:04 +0300

    2nd task

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-# Створіть клас черги для роботи із символьними зна- ченнями. Ви маєте створити реалізації для операцій над елементами:
+# Створіть клас черги з пріоритетами для роботи із символьними значеннями.
+# Ви маєте створити реалізації для операцій над еле- ментами черги:
 # ■ IsEmpty — перевірка, чи черга пуста;
 # ■ IsFull — перевірка черги на заповнення;
-# ■ Enqueue — додати новий елемент до черги;
-# ■ Dequeue — видалення елемента з черги;
-# ■ Show — відображення на екрані всіх елементів черги.
+# ■ InsertWithPriority — додати елемент з пріоритетом у
+# чергу;
+#
+# ■ PullHighestPriorityElement — видалення елемента з найвищим пріоритетом із черги;
+# ■ Peek — повернення найбільшого за пріоритетом еле- мента. Зверніть увагу, що елемент не видаляється з черги;
+# ■ Show — відображення на екрані всіх елементів черги. Показуючи елемент, також необхідно вказати і його пріоритет.
 # На старті додатка відобразіть меню, в якому корис- тувач може вибрати необхідну операцію.
 
 
-from queue import Queue
+
+from queue import PriorityQueue
 
 class Line:
     def __init__(self):
-        self.queue = Queue()
+        self.queue = PriorityQueue()
 
     def queue_is_empty(self):
         return self.queue.empty()
@@ -19,37 +24,46 @@ class Line:
     def queue_is_full(self):
         return self.queue.full()
 
-    def add_element(self, element):
-        self.queue.put(element)
 
-    def remove_element(self):
+    def add_element(self, element, priority):
+        self.queue.put((priority, element))
+
+    def delete_element(self):
+        if self.queue.empty():
+            print("Queue is emtpy")
+            return
+
+        priority, element = self.queue.get()
+
+        print(f'Element {element} is deleted')
+
+    def peek(self):
         if not self.queue.empty():
-            return self.queue.get()
+            priority, symbol = self.queue.queue[0]
+            print(f"Peek element: {symbol}")
         else:
-            return None
+            print("Queue is empty")
 
-    def print(self):
-        temp_queue = Queue()
-
-        while not self.queue.empty():
-            client = self.queue.get()
-            print(client, end=' <- ')
-            temp_queue.put(client)
-
+    def show(self):
+        if not self.queue.empty():
+            for priority, symbol in self.queue.queue:
+                print(priority, symbol)
+        else:
+            return False
 
 line = Line()
 print(line.queue_is_empty())
 print(line.queue_is_full())
 
-line.add_element(5)
+line.add_element(4, "A")
 print(line.queue_is_empty())
 
-
-line.remove_element()
+line.delete_element()
 print(line.queue_is_empty())
 
-line.add_element(3)
-line.add_element(2)
-line.remove_element()
-line.add_element(1)
-line.print()
+line.add_element(1, "B")
+line.add_element(4, "A")
+print(line.peek())
+
+line.show()
+
